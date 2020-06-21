674b1a6c9c50576beb30413fa94d077d57073430
yasuk-0714 <k.yasu.714@gmail.com>
2020-06-21 16:39:36 +0900

    deploy setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :basic_auth, if: :production?
+
+private
+
+def basic_auth
+  authenticate_or_request_with_http_basic do |username, password|
+    username == Rails.application.credentials[:basic_auth][:user] &&
+    password == Rails.application.credentials[:basic_auth][:pass]
+  end
+  def production?
+    Rails.env.production?
+  end
 end
+end
+
